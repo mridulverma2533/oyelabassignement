a078b4a33d663ff15276cea0df98cee059fcdb12
mridul <mridulverma2533@gmail.com>
2022-04-04 16:41:03 +0530

    mridul

--- controller/admincontroller.js
+++ controller/admincontroller.js
@@ -49,5 +49,27 @@ exports.register=async(req,res)=>{
      }catch(e){
          console.log(e);
      }
+ },
+ 
+ exports.findall=async(req,res)=>{
+     try{
+         const data = await User.find()
+         res.status(200).json(data)
+
+     }catch(e){
+         console.log(e);
+         res.send(e)
+     }
+ },
+ exports.find = async(req,res)=>{
+     try{
+         const _id = req.params.id
+         const data = await User.findOne({_id:_id})
+         res.status(200).json(data)
+
+     }catch(e){
+         console.log(e);
+         res.status(500).json(e)
+     }
  }
 
--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -5,6 +5,8 @@ const controller=require("../controller/admincontroller");
 router.post("/register",controller.register);
 router.post("/login",controller.login)
 router.post("/object",controller.object)
+router.get("/findall",controller.findall)
+router.get("/find/:id",controller.find)
 
 
 
